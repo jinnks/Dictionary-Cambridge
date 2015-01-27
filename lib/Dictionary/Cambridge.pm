119dad6e84162d667fdc6e849b04610b3dcc35c3
Farhan Siddiqui <farhan@farhan-dev>
2015-01-27 00:26:08 +0000

    initial commit

---
+++ lib/Dictionary/Cambridge.pm
@@ -0,0 +1,33 @@
+package Dictionary::Cambridge;
+
+use Moose;
+use DDP;
+use HTTP::Request;
+use LWP::UserAgent;
+use URI::Escape;
+
+has "base_url" =>(
+        is => 'ro',
+        isa => 'Str',
+        default => 'https://dictionary.cambridge.org/api/v1'
+);
+
+has "access_key" => (
+    is => 'rw',
+    isa => 'Str',
+);
+
+has "user_agent" => (
+    is => 'ro',
+    isa => 'LWP::UserAgent',
+    lazy_build => 1
+);
+
+has "header" => (
+    is => 'ro',
+    isa => 'HTTP::Request',
+    lazy_build => 1,
+);
+
+
+1;
