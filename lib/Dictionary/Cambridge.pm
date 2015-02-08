9eb18185510e4f71a4362bf3a43ca8768affbdf9
Farhan Siddiqui <farhan@farhan-dev>
2015-02-08 23:26:31 +0000

    first method to search is there

--- lib/Dictionary/Cambridge.pm
+++ lib/Dictionary/Cambridge.pm
@@ -4,12 +4,12 @@ use Moose;
 use DDP;
 use HTTP::Request;
 use LWP::UserAgent;
-use URI::Escape;
+use URI::Encode;
 
 has "base_url" =>(
         is => 'ro',
         isa => 'Str',
-        default => 'https://dictionary.cambridge.org/api/v1'
+        default => 'https://dictionary.cambridge.org/api/v1/'
 );
 
 has "access_key" => (
@@ -23,16 +23,50 @@ has "user_agent" => (
     lazy_build => 1
 );
 
-has "header" => (
+has "http_request" =>(
     is => 'ro',
     isa => 'HTTP::Request',
-    lazy_build => 1,
+    lazy_build => 1
 );
 
-has 'encode_uri' => (
-     is => 'ro',
-     isa => 'URI::Escape',
-     lazy_build => 1
+has "encode_uri" =>(
+    is =>'ro',
+    isa => 'URI::Encode',
+    lazy_build => 1
 );
+
+sub _build_user_agent {
+    
+    return LWP::UserAgent->new();
+}
+
+sub _build_http_request {
+    
+    return HTTP::Request->new(); 
+}
+
+sub _build_encode_uri {
+    
+    return URI::Encode->new();
+}
+
+sub get_entry {
+
+    my ($self, $dict_id, $entry_id) = @_;
+    #return an error message unless there is entry_id and dict_id
+    return "Dictionary id or Entry id not found" unless $dict_id and $entry_id;
+    $self->user_agent->default_header(accessKey => $self->access_key);
+    my $uri = $self->base_url;
+    $uri .= 'dictionaries/'.$dict_id.'/entries/';
+    $uri .=  $entry_id.'/?format=html';
+    $uri = $self->encode_uri->encode($uri);
+    p($uri);
+    print STDERR $uri."\n";
+    my $response = $self->user_agent->get($uri);
+    return $response;
+
+}
+
+
 
 1;
